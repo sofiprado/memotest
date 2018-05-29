493cc571e9eb47165aaabad2730d8235f80dad97
sofia <sofiaprado1@gmail.com>
2018-05-29 00:21:00 -0300

    indentation details

--- js/tp_memotest.js
+++ js/tp_memotest.js
@@ -13,6 +13,14 @@ let a = [
   'https://raw.githubusercontent.com/adafront/frontendV3/master/modulo3-js/ejercicios/TP2Memotest/recursos/graceHopper.jpg',
   ]
 
+let gameArray = []  //array a llenar cada dos clicks
+
+let gameObjects = []
+
+let findTheWinner = [] //según length indica el ganador 
+
+let turnos = 24; 
+
 //AGREGAR NOMBRE DE JUGADOR
 const userName = $('#userName')
 
@@ -21,7 +29,6 @@ function add() {
   const showUser = '<p class="userStyle">' + inputValue + '</p>'
 
   userName.append(showUser)
- 
 }
 
 //MOSTRAR TURNOS RESTANTES
@@ -32,95 +39,72 @@ function left() {
   counter.html(tickTock)
 }
 
-
 $('#nameInput').on('keypress', function(e) {
-    if (e.keyCode == 13) {
-        add()
-        $('#nameInput').addClass('hideMe')
-        $('#jugador').addClass('hideMe')
-    }
+  if (e.keyCode == 13) {
+    add()
+    $('#nameInput').addClass('hideMe')
+    $('#jugador').addClass('hideMe')
+  }
 })
 
 //DESORDENAR EL ARRAY DE IMÁGENES
-
 const desordenadas = a.sort(function() {
   return Math.random() - 0.5
 });
 
- 
-let gameArray = []  //ARRAY A LLENAR CADA 2 CLICKS
-
-let gameObjects = []
-
-let findTheWinner = [] 
-
-let turnos = 24;
-
+//CLICK DA VUELTA LA IMAGEN
 $('img').on('click', function(e) {
   const id = e.target.id
   const clickedIndex = $(this).index()
   const saveThisClick = $(this)
+
+  $('#' + id).attr('src', desordenadas[clickedIndex]) //REEMPLAZAR URL
   
-$('#' + id).attr('src', desordenadas[clickedIndex]) //REEMPLAZAR URL
-/*
-if (saveThisClick.hasClass('isClicked')) {
-  $(this).attr('src', "https://media.licdn.com/dms/image/C560BAQEDf5SaTOmTKg/company-logo_200_200/0?e=2126476800&v=beta&t=hD_7eqBx_noU3Mcf6qmFaK7kplLwxqd7YdzyC8NUYp4")
-}*/
+  turnos--
+  left()
 
-//DETECTAR PERDEDOR
-if (turnos == 0) {
-  alert('Game Over')
-  location.reload()
-}
+  if (turnos == 0) { //DETECTAR PERDEDOR
+    alert('Game Over')
+    location.reload()
+  }
+    
+  gameArray.push(desordenadas[clickedIndex])  //IMAGEN CLICKEADA SE PUSHEA AL ARRAY
 
-//$(this).addClass('isClicked');
-turnos--
-    //console.log(turnos)
-left()
-
-gameArray.push(desordenadas[clickedIndex])  //AGREGO IMAGENES CLICKEADAS A ARRAY
-
-gameObjects.push( {
-  imgIndex: clickedIndex,
-  clickedDiv: saveThisClick
-})
+  gameObjects.push({
+    imgIndex: clickedIndex,
+    clickedDiv: saveThisClick
+  })
    
-if (gameArray.length == 2) {  
- 
-  if (gameArray[0] == gameArray[1]) { //COMPARAR PAR DE CARTAS
+  if (gameArray.length == 2) {   
+    if (gameArray[0] == gameArray[1]) { //COMPARAR PAR DE CARTAS
     
-    findTheWinner.push(gameArray[0], gameArray[1])
-    gameArray = []
-    gameObjects = []
-    console.log(findTheWinner)
+      findTheWinner.push(gameArray[0], gameArray[1])
     
-    setTimeout(function() {
+      gameArray = []
+      gameObjects = []
+    
+      setTimeout(function() {
       const inputValue = $('#nameInput').val()
-    if (findTheWinner.length == 12) {
-      alert('Ganaste ' + inputValue + "!") 
-    }    
-  }, 1000)
+        if (findTheWinner.length == 12) {
+        alert('Ganaste ' + inputValue + "!") 
+      }    
+    }, 1000)
   
-
-  } else {
-    setTimeout(function(){
-   // saveThisClick.removeClass('isClicked')
+  } else { //VOLVER A IMG INICIAL
+    setTimeout(function(){  
       gameObjects[0].clickedDiv.attr('src', "imgs/blurred.png" )      
       gameObjects[1].clickedDiv.attr('src', "imgs/blurred.png" )
 
       gameArray = []
       gameObjects = []
     },900) 
-  } 
-}  
-//"https://media.licdn.com/dms/image/C560BAQEDf5SaTOmTKg/company-logo_200_200/0?e=2126476800&v=beta&t=hD_7eqBx_noU3Mcf6qmFaK7kplLwxqd7YdzyC8NUYp4"
-
-// console.log(id)
-//console.log(clickedIndex)
-//console.log(desordenadas[clickedIndex])
-// console.log(gameArray)
+   } 
+  }  
 })
 
 
  
-//usar data para guardar info
+
+//if (saveThisClick.hasClass('isClicked')) 
+//$(this).addClass('isClicked');
+ // saveThisClick.removeClass('isClicked')
